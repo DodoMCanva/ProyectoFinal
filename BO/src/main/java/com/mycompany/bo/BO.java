3a3ba9b6376c90ab6d677aaea917d3cb61a0c7d2
DodoMCanva <106896424+DodoMCanva@users.noreply.github.com>
2024-11-17 19:54:42 -0600

    Iniciar

---
+++ BO/src/main/java/com/mycompany/bo/BO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bo;
+
+/**
+ *
+ * @author Fer
+ */
+public class BO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
